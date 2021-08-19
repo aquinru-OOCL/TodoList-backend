76b68c225361d705d3b0d95b54dbb7c5a4975f3c
AQUINRU <russel.aquino@oocl.com>
2021-08-19 22:07:19 +0800

    [TodoBackend][Russel] feat: implement should_return_todos_when_find_all_todo

--- src/main/java/com/example/todolist/controller/TodoController.java
+++ src/main/java/com/example/todolist/controller/TodoController.java
@@ -2,6 +2,7 @@ package com.example.todolist.controller;
 
 import com.example.todolist.entity.Todo;
 import com.example.todolist.service.TodoService;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,6 +21,7 @@ public class TodoController {
         return todoService.getAllTodos();
     }
 
+    @ResponseStatus(code = HttpStatus.CREATED)
     @PostMapping
     public Todo addTodo(@RequestBody Todo todo) {
         return todoService.addTodo(todo);
