7b97fbd36fca0ca4f9d8d3e75537348a1bf2416e
AQUINRU <russel.aquino@oocl.com>
2021-08-19 14:45:49 +0800

    [TodoBackend][Russel] feat: implement get API

---
+++ src/main/java/com/example/todolist/controller/TodoController.java
@@ -0,0 +1,24 @@
+package com.example.todolist.controller;
+
+import com.example.todolist.entity.Todo;
+import com.example.todolist.service.TodoService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+    private TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @GetMapping
+    public List<Todo> getAllTodos() {
+        return todoService.getAllTodos();
+    }
+}
---
+++ src/main/java/com/example/todolist/service/TodoService.java
@@ -0,0 +1,18 @@
+package com.example.todolist.service;
+
+import com.example.todolist.entity.Todo;
+import com.example.todolist.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TodoService {
+    @Autowired
+    private TodoRepository todoRepository;
+
+    public List<Todo> getAllTodos() {
+        return todoRepository.findAll();
+    }
+}
